2dcc3f16fb69fb216a4a59540216e5554542b4a1
mhbadawy <125281279+mhbadawy@users.noreply.github.com>
2025-11-11 14:16:52 +0200

    Resolve Issues Identified in FSDP Experiments (#13)
    * fix conda yml file.
    
    * remove commented lines
    
    * replaced env_vars conda path with generic path.
    
    * fix nccl + add generic path
    
    * Review and fix slurm job scripts
    
    * Fix: set --mem=0 for 8 GPU run to allocate full node memory
    
    * reintroduced wandb logging.
    
    * reduce number of allocated gpus per node
    
    * reduced train size to 250 across files + wandb id
    
    * fixed exercise 1: peak system metrics + 5 epochs.
    
    * change wandb to online.
    
    * fix tables format
    
    * mixed precision instead of fp16.
    
    * add check for provided arg.
    
    * updated readme for wandb key insertion
    
    * updated readme for mixed precision.
    
    ---------
    
    Co-authored-by: youssef <youssef.ghazal@brightskiesinc.com>
    Co-authored-by: ahmedYSoliman <solimaay@kaust.edu.sa>
    Co-authored-by: tarnimat.hatem <tarnimat.hatem@brightskiesinc.com>
    Co-authored-by: fajr99 <talfajrh@gmail.com>

--- fsdp/custom_model/single_node/env_vars.sh
+++ fsdp/custom_model/single_node/env_vars.sh
@@ -1,23 +1,22 @@
 # Conda setup
-export CONDA_SH_PATH="/path/to/miniforge/etc/profile.d/conda.sh"
 export CONDA_ENV="bloom_env"
 
-# Wandb/offline‐run settings
+# Wandb/online‐run settings
 export EXPERIMENT_NAME="Custom_Model_Single_Node"
-export LOG_DIR="path/to/logs"
+export LOG_DIR="$PWD/logs/"
 export WANDB_API_KEY="your_wandb_api_key"
 
 
-export OUTPUT_DIR="/path/to/outputs/${EXPERIMENT_NAME}"
+export OUTPUT_DIR="outputs/${EXPERIMENT_NAME}"
 export MAX_LENGTH=512
-export TRAIN_SIZE=500
+export TRAIN_SIZE=250
 export EVAL_SIZE=100
 export NUM_EPOCHS=5
 export BATCH_SIZE=1
 export LEARNING_RATE=5e-5
 export WEIGHT_DECAY=0.01
 export GRAD_ACC=4
-export FP16=True
+export FP16=False
 export BF16=False
 
 # ── Model architecture ───────────────────────────────
