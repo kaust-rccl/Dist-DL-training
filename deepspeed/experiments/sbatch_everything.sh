668ae674419e7fbb317891297e9e1d94e1396d33
fajr99 <talfajrh@gmail.com>
2025-12-11 13:41:03 +0200

    comment out unwanted exp

--- deepspeed/experiments/sbatch_everything.sh
+++ deepspeed/experiments/sbatch_everything.sh
@@ -26,13 +26,13 @@ submit() {
 ################################################################################
 submit baseline/baseline.slurm
 
-################################################################################
-# SINGLE GPU (no offloading)
-################################################################################
-submit deepspeed-single-gpu/zero_0/deepspeed_zero0.slurm
-submit deepspeed-single-gpu/zero_1/deepspeed_zero1.slurm
-submit deepspeed-single-gpu/zero_2/deepspeed_zero2.slurm
-submit deepspeed-single-gpu/zero_3/deepspeed_zero3.slurm
+#################################################################################
+## SINGLE GPU (no offloading)
+#################################################################################
+#submit deepspeed-single-gpu/zero_0/deepspeed_zero0.slurm
+#submit deepspeed-single-gpu/zero_1/deepspeed_zero1.slurm
+#submit deepspeed-single-gpu/zero_2/deepspeed_zero2.slurm
+#submit deepspeed-single-gpu/zero_3/deepspeed_zero3.slurm
 
 ################################################################################
 # SINGLE GPU (CPU OFFLOADING)
@@ -52,10 +52,10 @@ submit deepspeed-multi-gpu/2_gpus_stages_comparison/zero_1/deepspeed_2_gpus_zero
 submit deepspeed-multi-gpu/2_gpus_stages_comparison/zero_2/deepspeed_2_gpus_zero2.slurm
 submit deepspeed-multi-gpu/2_gpus_stages_comparison/zero_3/deepspeed_2_gpus_zero3.slurm
 
-# Stages comparison 2 GPUs (offload)
-submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_1/deepspeed_2_gpus_zero1_offload.slurm
-submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_2/deepspeed_2_gpus_zero2_offload.slurm
-submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_3/deepspeed_2_gpus_zero3_offload.slurm
+## Stages comparison 2 GPUs (offload)
+#submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_1/deepspeed_2_gpus_zero1_offload.slurm
+#submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_2/deepspeed_2_gpus_zero2_offload.slurm
+#submit deepspeed-multi-gpu/2_gpus_stages_comparison/cpu_offloading/zero_3/deepspeed_2_gpus_zero3_offload.slurm
 
 # 4 GPUs
 submit deepspeed-multi-gpu/4_gpus/deepspeed_4_gpus.slurm
@@ -66,9 +66,9 @@ submit deepspeed-multi-gpu/8_gpus/deepspeed_8_gpus.slurm
 ################################################################################
 # MULTI-NODE
 ################################################################################
-submit deepspeed-multi-node/2_nodes/deepspeed_2_nodes.slurm
-submit deepspeed-multi-node/4_nodes/deepspeed_4_nodes.slurm
-submit deepspeed-multi-node/8_nodes/deepspeed_8_nodes.slurm
+#submit deepspeed-multi-node/2_nodes/deepspeed_2_nodes.slurm
+#submit deepspeed-multi-node/4_nodes/deepspeed_4_nodes.slurm
+#submit deepspeed-multi-node/8_nodes/deepspeed_8_nodes.slurm
 
 echo ""
 echo "==================== All jobs have been submitted ===================="
